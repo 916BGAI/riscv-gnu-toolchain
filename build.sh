b574df7dff07f2e22c98f8e682511861f95aeb1f
916BGAI <916772719@qq.com>
2023-08-23 17:33:20 +0800

    commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+./configure --prefix=/home/kjlsai/Applications/riscv-gnu-toolchain --with-m
+ultilib-generator="rv32im-ilp32--;rv32im_zicsr_zifencei-ilp32--;rv32imac-ilp32--;rv32imafc-ilp32f--;rv32ec-ilp32e--"
+
+make newlib -j16
+
