422af9a17d923622629c5108a4c5b36076071b0a
916BGAI <916772719@qq.com>
2023-08-24 20:34:34 +0800

    commit

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-./configure --prefix=/home/kjlsai/Applications/riscv-gnu-toolchain --with-multilib-generator="rv32im-ilp32--;rv32im_zicsr_zifencei-ilp32--;rv32imac-ilp32--;rv32imafc-ilp32f--;rv32ec-ilp32e--"
+./configure --prefix=/home/kjlsai/Applications/riscv-gnu-toolchain --with-multilib-generator="rv32im-ilp32--;rv32im_zicsr_zifencei-ilp32--;rv32imac-ilp32--;rv32imac_zicsr_zifencei-ilp32--;rv32imafc-ilp32f--;rv32imafc_zicsr_zifencei-ilp32f--;rv32ec-ilp32e--;rv32ec_zicsr_zifencei-ilp32e--"
 
 make newlib -j16
 
